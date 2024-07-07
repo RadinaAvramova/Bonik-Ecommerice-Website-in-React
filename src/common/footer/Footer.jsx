df8a335dc2902b3b6b10116dd01686c69b1ba338
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-07-07 15:27:13 +0300

    Update Footer.jsx

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='container grid2'>
           <div className='box'>
             <h1>Bonik</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
+            <p></p>
             <div className='icon d_flex'>
               <div className='img d_flex'>
                 <i class='fa-brands fa-google-play'></i>
